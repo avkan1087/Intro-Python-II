5cbbcd4787f6deca783c6bdf9be7b6eaf98d1209
avi <avkan1087@gmail.com>
2020-04-09 14:23:15 -0400

    add item to player

--- src/player.py
+++ src/player.py
@@ -3,5 +3,19 @@
 
 
 class Player:
-    def __init__(self, current_room):
+    def __init__(self, current_room, item=[]):
         self.current_room = current_room
+        self.item = item
+
+    def __str__(self):
+        x = f"Your Current Location: {self.current_room.name}\n Current Inventory: "
+        if len(self.item)<1:
+            x += f"Nothing"
+        else:
+            for i in self.item:
+                x += f"\n {i}"
+        return x
+
+    def take(self, item="na"):
+        self.item.append(item)
+        return f"You Chose {item}"
